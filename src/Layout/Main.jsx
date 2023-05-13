319d9fa3752ea64c18993e84f038980e67ae0d5f
Ziaul-hasan <ziaulhasanshaon006@gmail.com>
2023-05-13 15:50:45 +0600

    basic router set

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Header from '../Page/Shared/Header/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Page/Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
